dab5eff4c51535c918066c4f9fe36cf01f9a2642
Jason Sears <jsears4585@gmail.com>
2017-06-29 14:12:05 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-  // call functions here
+$(document).ready(function() {
+  func()
 });
 
 // define functions here
+var func = function() {
+  $('form').on('submit', function(e) {
+    var item = $('#item').val()
+    $("#list ol").append(`<li>${item}</li>`)
+    e.preventDefault()
+  })
+}
